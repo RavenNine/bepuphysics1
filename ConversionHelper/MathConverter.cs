8b44ee8be0a5a57941949f0e5db5f35319a68a30
Dave Rooke <dave@ravenixstudio.co.uk>
2024-09-25 11:19:28 +0100

    .Net 8.0

--- ConversionHelper/MathConverter.cs
+++ ConversionHelper/MathConverter.cs
@@ -1,137 +1,137 @@
-using Microsoft.Xna.Framework;
+using System.Numerics;
 
 namespace ConversionHelper
 {
     /// <summary>
-    /// Helps convert between XNA math types and the BEPUphysics replacement math types.
+    /// Helps convert between Numerics math types and the BEPUphysics replacement math types.
     /// A version of this converter could be created for other platforms to ease the integration of the engine.
     /// </summary>
     public static class MathConverter
     {
-        //Vector2
-        public static Vector2 Convert(BEPUutilities.Vector2 bepuVector)
+        //NVector2
+        public static NVector2 Convert(BVector2 bepuVector)
         {
-            Vector2 toReturn;
+            NVector2 toReturn;
             toReturn.X = bepuVector.X;
             toReturn.Y = bepuVector.Y;
             return toReturn;
         }
 
-        public static void Convert(ref BEPUutilities.Vector2 bepuVector, out Vector2 xnaVector)
+        public static void Convert(ref BVector2 bepuVector, out NVector2 numericsVector)
         {
-            xnaVector.X = bepuVector.X;
-            xnaVector.Y = bepuVector.Y;
+            numericsVector.X = bepuVector.X;
+            numericsVector.Y = bepuVector.Y;
         }
 
-        public static BEPUutilities.Vector2 Convert(Vector2 xnaVector)
+        public static BVector2 Convert(NVector2 numericsVector)
         {
-            BEPUutilities.Vector2 toReturn;
-            toReturn.X = xnaVector.X;
-            toReturn.Y = xnaVector.Y;
+            BVector2 toReturn;
+            toReturn.X = numericsVector.X;
+            toReturn.Y = numericsVector.Y;
             return toReturn;
         }
 
-        public static void Convert(ref Vector2 xnaVector, out BEPUutilities.Vector2 bepuVector)
+        public static void Convert(ref NVector2 numericsVector, out BVector2 bepuVector)
         {
-            bepuVector.X = xnaVector.X;
-            bepuVector.Y = xnaVector.Y;
+            bepuVector.X = numericsVector.X;
+            bepuVector.Y = numericsVector.Y;
         }
 
-        //Vector3
-        public static Vector3 Convert(BEPUutilities.Vector3 bepuVector)
+        //NVector3
+        public static NVector3 Convert(BVector3 bepuVector)
         {
-            Vector3 toReturn;
+            NVector3 toReturn;
             toReturn.X = bepuVector.X;
             toReturn.Y = bepuVector.Y;
             toReturn.Z = bepuVector.Z;
             return toReturn;
         }
 
-        public static void Convert(ref BEPUutilities.Vector3 bepuVector, out Vector3 xnaVector)
+        public static void Convert(ref BVector3 bepuVector, out NVector3 numericsVector)
         {
-            xnaVector.X = bepuVector.X;
-            xnaVector.Y = bepuVector.Y;
-            xnaVector.Z = bepuVector.Z;
+            numericsVector.X = bepuVector.X;
+            numericsVector.Y = bepuVector.Y;
+            numericsVector.Z = bepuVector.Z;
         }
 
-        public static BEPUutilities.Vector3 Convert(Vector3 xnaVector)
+        public static BVector3 Convert(NVector3 numericsVector)
         {
-            BEPUutilities.Vector3 toReturn;
-            toReturn.X = xnaVector.X;
-            toReturn.Y = xnaVector.Y;
-            toReturn.Z = xnaVector.Z;
+            BVector3 toReturn;
+            toReturn.X = numericsVector.X;
+            toReturn.Y = numericsVector.Y;
+            toReturn.Z = numericsVector.Z;
             return toReturn;
         }
 
-        public static void Convert(ref Vector3 xnaVector, out BEPUutilities.Vector3 bepuVector)
+        public static void Convert(ref NVector3 numericsVector, out BVector3 bepuVector)
         {
-            bepuVector.X = xnaVector.X;
-            bepuVector.Y = xnaVector.Y;
-            bepuVector.Z = xnaVector.Z;
+            bepuVector.X = numericsVector.X;
+            bepuVector.Y = numericsVector.Y;
+            bepuVector.Z = numericsVector.Z;
         }
 
-        public static Vector3[] Convert(BEPUutilities.Vector3[] bepuVectors)
+        public static NVector3[] Convert(BVector3[] bepuVectors)
         {
-            Vector3[] xnaVectors = new Vector3[bepuVectors.Length];
+            NVector3[] numericsVectors = new NVector3[bepuVectors.Length];
             for (int i = 0; i < bepuVectors.Length; i++)
             {
-                Convert(ref bepuVectors[i], out xnaVectors[i]);
+                Convert(ref bepuVectors[i], out numericsVectors[i]);
             }
-            return xnaVectors;
+            return numericsVectors;
 
         }
 
-        public static BEPUutilities.Vector3[] Convert(Vector3[] xnaVectors)
+        public static BVector3[] Convert(NVector3[] numericsVectors)
         {
-            var bepuVectors = new BEPUutilities.Vector3[xnaVectors.Length];
-            for (int i = 0; i < xnaVectors.Length; i++)
+            var bepuVectors = new BVector3[numericsVectors.Length];
+            for (int i = 0; i < numericsVectors.Length; i++)
             {
-                Convert(ref xnaVectors[i], out bepuVectors[i]);
+                Convert(ref numericsVectors[i], out bepuVectors[i]);
             }
             return bepuVectors;
 
         }
 
-        //Matrix
-        public static Matrix Convert(BEPUutilities.Matrix matrix)
+        //NMatrix
+        public static NMatrix Convert(BMatrix matrix)
         {
-            Matrix toReturn;
+            NMatrix toReturn;
             Convert(ref matrix, out toReturn);
             return toReturn;
         }
 
-        public static BEPUutilities.Matrix Convert(Matrix matrix)
+        public static BMatrix Convert(NMatrix matrix)
         {
-            BEPUutilities.Matrix toReturn;
+            BMatrix toReturn;
             Convert(ref matrix, out toReturn);
             return toReturn;
         }
 
-        public static void Convert(ref BEPUutilities.Matrix matrix, out Matrix xnaMatrix)
+        public static void Convert(ref BMatrix matrix, out NMatrix numericsMatrix)
         {
-            xnaMatrix.M11 = matrix.M11;
-            xnaMatrix.M12 = matrix.M12;
-            xnaMatrix.M13 = matrix.M13;
-            xnaMatrix.M14 = matrix.M14;
+            numericsMatrix.M11 = matrix.M11;
+            numericsMatrix.M12 = matrix.M12;
+            numericsMatrix.M13 = matrix.M13;
+            numericsMatrix.M14 = matrix.M14;
 
-            xnaMatrix.M21 = matrix.M21;
-            xnaMatrix.M22 = matrix.M22;
-            xnaMatrix.M23 = matrix.M23;
-            xnaMatrix.M24 = matrix.M24;
+            numericsMatrix.M21 = matrix.M21;
+            numericsMatrix.M22 = matrix.M22;
+            numericsMatrix.M23 = matrix.M23;
+            numericsMatrix.M24 = matrix.M24;
 
-            xnaMatrix.M31 = matrix.M31;
-            xnaMatrix.M32 = matrix.M32;
-            xnaMatrix.M33 = matrix.M33;
-            xnaMatrix.M34 = matrix.M34;
+            numericsMatrix.M31 = matrix.M31;
+            numericsMatrix.M32 = matrix.M32;
+            numericsMatrix.M33 = matrix.M33;
+            numericsMatrix.M34 = matrix.M34;
 
-            xnaMatrix.M41 = matrix.M41;
-            xnaMatrix.M42 = matrix.M42;
-            xnaMatrix.M43 = matrix.M43;
-            xnaMatrix.M44 = matrix.M44;
+            numericsMatrix.M41 = matrix.M41;
+            numericsMatrix.M42 = matrix.M42;
+            numericsMatrix.M43 = matrix.M43;
+            numericsMatrix.M44 = matrix.M44;
 
         }
 
-        public static void Convert(ref Matrix matrix, out BEPUutilities.Matrix bepuMatrix)
+        public static void Convert(ref NMatrix matrix, out BMatrix bepuMatrix)
         {
             bepuMatrix.M11 = matrix.M11;
             bepuMatrix.M12 = matrix.M12;
@@ -155,37 +155,37 @@ namespace ConversionHelper
 
         }
 
-        public static Matrix Convert(BEPUutilities.Matrix3x3 matrix)
+        public static NMatrix Convert(BMatrix3x3 matrix)
         {
-            Matrix toReturn;
+            NMatrix toReturn;
             Convert(ref matrix, out toReturn);
             return toReturn;
         }
 
-        public static void Convert(ref BEPUutilities.Matrix3x3 matrix, out Matrix xnaMatrix)
+        public static void Convert(ref BMatrix3x3 matrix, out NMatrix numericsMatrix)
         {
-            xnaMatrix.M11 = matrix.M11;
-            xnaMatrix.M12 = matrix.M12;
-            xnaMatrix.M13 = matrix.M13;
-            xnaMatrix.M14 = 0;
+            numericsMatrix.M11 = matrix.M11;
+            numericsMatrix.M12 = matrix.M12;
+            numericsMatrix.M13 = matrix.M13;
+            numericsMatrix.M14 = 0;
 
-            xnaMatrix.M21 = matrix.M21;
-            xnaMatrix.M22 = matrix.M22;
-            xnaMatrix.M23 = matrix.M23;
-            xnaMatrix.M24 = 0;
+            numericsMatrix.M21 = matrix.M21;
+            numericsMatrix.M22 = matrix.M22;
+            numericsMatrix.M23 = matrix.M23;
+            numericsMatrix.M24 = 0;
 
-            xnaMatrix.M31 = matrix.M31;
-            xnaMatrix.M32 = matrix.M32;
-            xnaMatrix.M33 = matrix.M33;
-            xnaMatrix.M34 = 0;
+            numericsMatrix.M31 = matrix.M31;
+            numericsMatrix.M32 = matrix.M32;
+            numericsMatrix.M33 = matrix.M33;
+            numericsMatrix.M34 = 0;
 
-            xnaMatrix.M41 = 0;
-            xnaMatrix.M42 = 0;
-            xnaMatrix.M43 = 0;
-            xnaMatrix.M44 = 1;
+            numericsMatrix.M41 = 0;
+            numericsMatrix.M42 = 0;
+            numericsMatrix.M43 = 0;
+            numericsMatrix.M44 = 1;
         }
 
-        public static void Convert(ref Matrix matrix, out BEPUutilities.Matrix3x3 bepuMatrix)
+        public static void Convert(ref NMatrix matrix, out BMatrix3x3 bepuMatrix)
         {
             bepuMatrix.M11 = matrix.M11;
             bepuMatrix.M12 = matrix.M12;
@@ -201,10 +201,10 @@ namespace ConversionHelper
 
         }
 
-        //Quaternion
-        public static Quaternion Convert(BEPUutilities.Quaternion quaternion)
+        //NQuaternion
+        public static NQuaternion Convert(BQuaternion quaternion)
         {
-            Quaternion toReturn;
+            NQuaternion toReturn;
             toReturn.X = quaternion.X;
             toReturn.Y = quaternion.Y;
             toReturn.Z = quaternion.Z;
@@ -212,9 +212,9 @@ namespace ConversionHelper
             return toReturn;
         }
 
-        public static BEPUutilities.Quaternion Convert(Quaternion quaternion)
+        public static BQuaternion Convert(NQuaternion quaternion)
         {
-            BEPUutilities.Quaternion toReturn;
+            BQuaternion toReturn;
             toReturn.X = quaternion.X;
             toReturn.Y = quaternion.Y;
             toReturn.Z = quaternion.Z;
@@ -222,7 +222,7 @@ namespace ConversionHelper
             return toReturn;
         }
 
-        public static void Convert(ref BEPUutilities.Quaternion bepuQuaternion, out Quaternion quaternion)
+        public static void Convert(ref BQuaternion bepuQuaternion, out NQuaternion quaternion)
         {
             quaternion.X = bepuQuaternion.X;
             quaternion.Y = bepuQuaternion.Y;
@@ -230,128 +230,12 @@ namespace ConversionHelper
             quaternion.W = bepuQuaternion.W;
         }
 
-        public static void Convert(ref Quaternion quaternion, out  BEPUutilities.Quaternion bepuQuaternion)
+        public static void Convert(ref NQuaternion quaternion, out  BQuaternion bepuQuaternion)
         {
             bepuQuaternion.X = quaternion.X;
             bepuQuaternion.Y = quaternion.Y;
             bepuQuaternion.Z = quaternion.Z;
             bepuQuaternion.W = quaternion.W;
-        }
-
-        //Ray
-        public static BEPUutilities.Ray Convert(Ray ray)
-        {
-            BEPUutilities.Ray toReturn;
-            Convert(ref ray.Position, out toReturn.Position);
-            Convert(ref ray.Direction, out toReturn.Direction);
-            return toReturn;
-        }
-
-        public static void Convert(ref Ray ray, out BEPUutilities.Ray bepuRay)
-        {
-            Convert(ref ray.Position, out bepuRay.Position);
-            Convert(ref ray.Direction, out bepuRay.Direction);
-        }
-
-        public static Ray Convert(BEPUutilities.Ray ray)
-        {
-            Ray toReturn;
-            Convert(ref ray.Position, out toReturn.Position);
-            Convert(ref ray.Direction, out toReturn.Direction);
-            return toReturn;
-        }
-
-        public static void Convert(ref BEPUutilities.Ray ray, out Ray xnaRay)
-        {
-            Convert(ref ray.Position, out xnaRay.Position);
-            Convert(ref ray.Direction, out xnaRay.Direction);
-        }
-
-        //BoundingBox
-        public static BoundingBox Convert(BEPUutilities.BoundingBox boundingBox)
-        {
-            BoundingBox toReturn;
-            Convert(ref boundingBox.Min, out toReturn.Min);
-            Convert(ref boundingBox.Max, out toReturn.Max);
-            return toReturn;
-        }
-
-        public static BEPUutilities.BoundingBox Convert(BoundingBox boundingBox)
-        {
-            BEPUutilities.BoundingBox toReturn;
-            Convert(ref boundingBox.Min, out toReturn.Min);
-            Convert(ref boundingBox.Max, out toReturn.Max);
-            return toReturn;
-        }
-
-        public static void Convert(ref BEPUutilities.BoundingBox boundingBox, out BoundingBox xnaBoundingBox)
-        {
-            Convert(ref boundingBox.Min, out xnaBoundingBox.Min);
-            Convert(ref boundingBox.Max, out xnaBoundingBox.Max);
-        }
-
-        public static void Convert(ref BoundingBox boundingBox, out BEPUutilities.BoundingBox bepuBoundingBox)
-        {
-            Convert(ref boundingBox.Min, out bepuBoundingBox.Min);
-            Convert(ref boundingBox.Max, out bepuBoundingBox.Max);
-        }
-
-        //BoundingSphere
-        public static BoundingSphere Convert(BEPUutilities.BoundingSphere boundingSphere)
-        {
-            BoundingSphere toReturn;
-            Convert(ref boundingSphere.Center, out toReturn.Center);
-            toReturn.Radius = boundingSphere.Radius;
-            return toReturn;
-        }
-
-        public static BEPUutilities.BoundingSphere Convert(BoundingSphere boundingSphere)
-        {
-            BEPUutilities.BoundingSphere toReturn;
-            Convert(ref boundingSphere.Center, out toReturn.Center);
-            toReturn.Radius = boundingSphere.Radius;
-            return toReturn;
-        }
-
-        public static void Convert(ref BEPUutilities.BoundingSphere boundingSphere, out BoundingSphere xnaBoundingSphere)
-        {
-            Convert(ref boundingSphere.Center, out xnaBoundingSphere.Center);
-            xnaBoundingSphere.Radius = boundingSphere.Radius;
-        }
-
-        public static void Convert(ref BoundingSphere boundingSphere, out BEPUutilities.BoundingSphere bepuBoundingSphere)
-        {
-            Convert(ref boundingSphere.Center, out bepuBoundingSphere.Center);
-            bepuBoundingSphere.Radius = boundingSphere.Radius;
-        }
-
-        //Plane
-        public static Plane Convert(BEPUutilities.Plane plane)
-        {
-            Plane toReturn;
-            Convert(ref plane.Normal, out toReturn.Normal);
-            toReturn.D = plane.D;
-            return toReturn;
-        }
-
-        public static BEPUutilities.Plane Convert(Plane plane)
-        {
-            BEPUutilities.Plane toReturn;
-            Convert(ref plane.Normal, out toReturn.Normal);
-            toReturn.D = plane.D;
-            return toReturn;
-        }
-
-        public static void Convert(ref BEPUutilities.Plane plane, out Plane xnaPlane)
-        {
-            Convert(ref plane.Normal, out xnaPlane.Normal);
-            xnaPlane.D = plane.D;
-        }
-
-        public static void Convert(ref Plane plane, out BEPUutilities.Plane bepuPlane)
-        {
-            Convert(ref plane.Normal, out bepuPlane.Normal);
-            bepuPlane.D = plane.D;
         }
     }
 }
--- ConversionHelper/Properties/AssemblyInfo.cs
+++ ConversionHelper/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ConversionHelper")]
-[assembly: AssemblyDescription("Helps convert types between BEPUutilities and MonoGame.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bepu Entertainment LLC")]
-[assembly: AssemblyProduct("ConversionHelper")]
-[assembly: AssemblyCopyright("Copyright © 2018, Bepu Entertainment LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("aa264458-f33c-4b6b-90c1-b3209470893b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.1.0")]
-[assembly: AssemblyFileVersion("0")]
